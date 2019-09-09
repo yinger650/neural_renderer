7c4df4b6d21767c2444700847d2452bec5a0a435
yinger650 <yinger650@gmail.com>
2019-09-09 21:44:55 +0800

    add different projection methods

--- neural_renderer/projection.py
+++ neural_renderer/projection.py
@@ -41,3 +41,87 @@ def projection(vertices, K, R, t, dist_coeffs, orig_size, eps=1e-9):
     v = 2 * (v - orig_size / 2.) / orig_size
     vertices = torch.stack([u, v, z], dim=-1)
     return vertices
+
+def projection_P(vertices, P, dist_coeffs, orig_size):
+    '''
+    Calculate projective transformation of vertices given a projection matrix
+    P: 3x4 projection matrix
+    dist_coeffs: vector of distortion coefficients
+    orig_size: original size of image captured by the camera
+    '''
+    vertices = torch.cat([vertices, torch.ones_like(vertices[:, :, None, 0])], dim=-1)
+    vertices = torch.bmm(vertices, P.transpose(2, 1))
+    x, y, z = vertices[:, :, 0], vertices[:, :, 1], vertices[:, :, 2]
+    x_ = x / (z + 1e-5)
+    y_ = y / (z + 1e-5)
+
+    if dist_coeffs is None:
+        dist_coeffs = torch.tensor([[0., 0., 0., 0., 0.]], dtype=P.dtype, device=P.device).repeat(P.shape[0], 1)
+    # Get distortion coefficients from vector
+    k1 = dist_coeffs[:, None, 0]
+    k2 = dist_coeffs[:, None, 1]
+    p1 = dist_coeffs[:, None, 2]
+    p2 = dist_coeffs[:, None, 3]
+    k3 = dist_coeffs[:, None, 4]
+
+    # we use x_ for x' and x__ for x'' etc.
+    r = torch.sqrt(x_ ** 2 + y_ ** 2)
+    x__ = x_ * (1 + k1 * (r ** 2) + k2 * (r ** 4) + k3 * (r ** 6)) + 2 * p1 * x_ * y_ + p2 * (r ** 2 + 2 * x_ ** 2)
+    y__ = y_ * (1 + k1 * (r ** 2) + k2 * (r ** 4) + k3 * (r ** 6)) + p1 * (r ** 2 + 2 * y_ ** 2) + 2 * p2 * x_ * y_
+    x__ = 2 * (x__ - orig_size / 2.) / orig_size
+    y__ = 2 * (y__ - orig_size / 2.) / orig_size
+    vertices = torch.stack([x__, y__, z], dim=-1)
+    return vertices
+
+
+def projection_bbox(vertices, K, dist_coeffs, bbox, eps=1e-9):
+    """
+    :param vertices: b*#v*3
+    :param K: b*3*3
+    :param dist_coeffs: b*5
+    :param bbox: b*3 (x, y, size)
+    :param tar_size: float
+    :return:
+    """
+    '''
+        Calculate projective transformation of vertices given a projection matrix
+        Input parameters:
+        K: batch_size * 3 * 3 intrinsic camera matrix
+        R, t: batch_size * 3 * 3, batch_size * 1 * 3 extrinsic calibration parameters
+        dist_coeffs: vector of distortion coefficients
+        orig_size: original size of image captured by the camera
+        Returns: For each point [X,Y,Z] in world coordinates [u,v,z] where u,v are the coordinates of the projection in
+        pixels and z is the depth
+        '''
+
+    # instead of P*x we compute x'*P'
+    x, y, z = vertices[:, :, 0], vertices[:, :, 1], vertices[:, :, 2]
+    x_ = x / (z + eps)
+    y_ = y / (z + eps)
+
+    # Get distortion coefficients from vector
+    k1 = dist_coeffs[:, None, 0]
+    k2 = dist_coeffs[:, None, 1]
+    p1 = dist_coeffs[:, None, 2]
+    p2 = dist_coeffs[:, None, 3]
+    k3 = dist_coeffs[:, None, 4]
+
+    # we use x_ for x' and x__ for x'' etc.
+    r = torch.sqrt(x_ ** 2 + y_ ** 2)
+    x__ = x_ * (1 + k1 * (r ** 2) + k2 * (r ** 4) + k3 * (r ** 6)) + 2 * p1 * x_ * y_ + p2 * (r ** 2 + 2 * x_ ** 2)
+    y__ = y_ * (1 + k1 * (r ** 2) + k2 * (r ** 4) + k3 * (r ** 6)) + p1 * (r ** 2 + 2 * y_ ** 2) + 2 * p2 * x_ * y_
+    vertices = torch.stack([x__, y__, torch.ones_like(z)], dim=-1)
+    vertices = torch.matmul(vertices, K.transpose(1, 2))
+    u, v = vertices[:, :, 0], vertices[:, :, 1]
+
+    # shape is b*1
+    bx = bbox[:, None, 0]
+    by = bbox[:, None, 1]
+    bs_half = bbox[:, None, 2] * 0.5
+
+    u = (u - bx) / bs_half
+    v = (v - by) / bs_half
+    # z = z * 0.001
+
+    vertices = torch.stack([u, v, z], dim=-1)
+    return vertices
